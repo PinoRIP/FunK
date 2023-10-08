476447eb42a3c95ef51c6a69cf550ed50bbd3d83
PinoRIP <ItsPinoRIP@gmail.com>
2023-10-08 02:36:40 +0200

    Feature - Added wait for trigger

---
+++ Source/FunK/Public/Util/FunKBlueprintAsyncWaitForTrigger.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "FunKBlueprintAsyncWaitForTrigger.generated.h"
+
+class ATriggerBase;
+class AFunKTestBase;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FFunKTriggerTriggered, AActor*, Actor);
+
+/**
+ * 
+ */
+UCLASS()
+class FUNK_API UFunKBlueprintAsyncWaitForTrigger : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(BlueprintAssignable)
+	FFunKTriggerTriggered OnTriggered;
+	
+	UFUNCTION(BlueprintCallable, meta = (HidePin = "Test", DefaultToSelf = "Test", BlueprintInternalUseOnly = "true"), Category = "AsyncNode", DisplayName="WaitFor (Trigger)")
+	static UFunKBlueprintAsyncWaitForTrigger* WaitFor(AFunKTestBase* Test, ATriggerBase* Trigger);
+	
+	virtual void Activate() override;
+
+	virtual void SetReadyToDestroy() override;
+
+private:
+	UPROPERTY()
+	ATriggerBase* Trigger = nullptr;
+	
+	UFUNCTION()
+	void OnTriggeredActorBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
+};
